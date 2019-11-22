316083e801f034c084329866bd018e36bbe4ba05
Kugatov Maxim <maximkugatov@gmail.com>
2019-11-22 06:50:36 +0200

    fiat: added create image

--- course-04/exercises/lesson-3/starter-code/src/lambda/http/createImage.ts
+++ course-04/exercises/lesson-3/starter-code/src/lambda/http/createImage.ts
@@ -1,6 +1,7 @@
 import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
 import * as AWS  from 'aws-sdk'
+import * as uuid from 'uuid'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 
@@ -24,14 +25,27 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
     }
   }
 
-  // TODO: Create an image
+  const imageId = uuid.v4()
+  const parsedBody = JSON.parse(event.body)
+
+  const newItem = {
+    groupId: groupId,
+    imageId: imageId,
+    timestamp: Date.now(),
+    ...parsedBody
+  }
+
+  await docClient.put({
+    TableName: imagesTable,
+    Item: newItem
+  }).promise()
 
   return {
     statusCode: 201,
     headers: {
       'Access-Control-Allow-Origin': '*'
     },
-    body: ''
+    body: JSON.stringify({newItem})
   }
 }
 
